4cdefca118a8b3b296c1062f3a346b8d762689eb
Aleksi Romppainen <aromppa@gmail.com>
2026-05-17 18:50:55 +0300

    Add initial version of database schema

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE recipe (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    created_at TEXT NOT NULL,
+    title TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    glass TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+CREATE TABLE review (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    recipe_id INTEGER NOT NULL,
+    created_at TEXT NOT NULL,
+    content TEXT NOT NULL,
+    rating INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (recipe_id) REFERENCES recipe (id)
+);
+
+CREATE TABLE tag (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE recipe_tag (
+    recipe_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    PRIMARY KEY (recipe_id, tag_id)
+);
